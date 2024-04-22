f9a1a4bc6f697bb597c254362366f5c40d18eeed
farrelarrizal <mfarrel1301@gmail.com>
2024-04-22 22:44:16 +0700

    feat(dashboard): add sidebar

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,18 @@
+<!-- [ Sidebar Menu ] start -->
+<nav class="pc-sidebar">
+  <div class="navbar-wrapper">
+    <div class="m-header">
+      <a href="#" class="b-brand text-primary">
+        <!-- ========   Change your logo from here   ============ -->
+        <img src="<?= asset('../assets/images/logo-dark.svg') ?>" />
+        <span class="badge bg-light-success rounded-pill ms-2 theme-version"></span>
+      </a>
+    </div>
+    <div class="navbar-content">
+      <ul class="pc-navbar">
+        @include('partials/menu-list')
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- [ Sidebar Menu ] end -->
