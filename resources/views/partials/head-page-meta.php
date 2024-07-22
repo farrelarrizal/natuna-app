894b5c9d7c4c7fcfdbb23cf3ab9aed6056e8cf49
farrelarrizal <mfarrel1301@gmail.com>
2024-07-23 00:31:17 +0700

    fix

--- resources/views/partials/head-page-meta.php
+++ resources/views/partials/head-page-meta.php
@@ -3,7 +3,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Buah nangka buah kedondong Belinya di pasar atom Minal Aidzin Wal Faidzin Mohon Maaf Lahir dan Batin">
+<meta name="description" content="ABMAS NATUNA ITS">
 <meta name="keywords" content="">
 <meta name="author" content="Phoenixcoded">
 
