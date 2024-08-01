428f527a08735ca9de281fc025693f6d4dc85dff
putriisabellaa <putriisabella1007@gmail.com>
2024-08-01 22:10:40 +0700

    Add logout function

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 // Auth
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'loginPost'])->name('login.post');
-Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 // protected by auth middleware
 Route::middleware('auth')->group(function () {
